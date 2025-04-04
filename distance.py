3aa359e0b7155d4caec909f046efd7db0ac242c3
Benjamin Gravell <bjgravell@gmail.com>
2025-04-04 07:27:13 +0200

    fx bug in compute_distance_matrix

--- distance.py
+++ distance.py
@@ -15,9 +15,18 @@ def compute_distance_matrix(X: ArrU8, Y: ArrU8) -> ArrU8:
     D = np.zeros((nx, ny), dtype=np.uint8)
     for i in prange(nx):
         for j in prange(ny):
+            # Reset accumulator
+            acc = 0
             for k in prange(nz):
-                # Use integer division by nz to ensure
-                # 1. The summand is integer valued
-                # 2. The sum does not exceed the size of np.uint8 in any entry of D
-                D[i, j] += abs(X[i, k] - Y[j, k]) // nz
+                # Use custom branching logic for the absolute difference
+                # to prevent underflow that would happen with uint8 subtraction.
+                x = X[i, k]
+                y = Y[j, k]
+                d = y - x if y > x else x - y
+                acc += d
+            # Use integer division by nz to ensure
+            # 1. The summand is integer valued
+            # 2. The sum does not exceed the size of np.uint8 in any entry of D
+            # 3. Computation happens fast
+            D[i, j] = np.uint8(acc // nz)
     return D
